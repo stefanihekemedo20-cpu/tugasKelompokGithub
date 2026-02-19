c6f622e712de5a6ab57632c492564d0110cf668b
ASUS <ASUS@LAPTOP-KD4KQJIO>
2026-02-19 20:59:02 +0700

    second commit

---
+++ src/tugasgithub/TugasGithub.java
@@ -0,0 +1,16 @@
+
+package tugasgithub;
+
+public class TugasGithub {
+
+    public static void main(String[] args) {
+     int[] nilai = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        double jumlah = 0;
+        
+        for (int i = 0; i < 10; i++) {
+            jumlah = jumlah + nilai[i];
+        }
+        System.out.println(jumlah);
+    }
+    
+}
