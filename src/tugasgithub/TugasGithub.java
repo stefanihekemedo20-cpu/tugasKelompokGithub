65dff148bdad284b8d7cdb2c381ff4db9fce4292
ASUS <ASUS@sarah>
2026-02-19 21:25:10 +0700

    tirth commit

--- src/tugasgithub/TugasGithub.java
+++ src/tugasgithub/TugasGithub.java
@@ -5,12 +5,13 @@ public class TugasGithub {
 
     public static void main(String[] args) {
      int[] nilai = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        double jumlah = 0;
+        double jumlah = 0, rerata;
         
         for (int i = 0; i < 10; i++) {
             jumlah = jumlah + nilai[i];
         }
-        System.out.println(jumlah);
+       rerata=jumlah/nilai.length;
+        System.out.println("rerata nilai = "+rerata);
     }
     
 }
